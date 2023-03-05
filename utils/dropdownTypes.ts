e651cd4bf0c2f26aa50247cbe93777d90f7cd9fe
Nerko Dev <akshatmehta_14434@mpsjaipur.com>
2023-03-05 21:18:41 +0530

    Update dropdownTypes.ts

--- utils/dropdownTypes.ts
+++ utils/dropdownTypes.ts
@@ -11,6 +11,7 @@ export type roomType =
   | "Bedroom"
   | "Bathroom"
   | "Office"
+  | "Kitchen"
   | "Gaming Room";
 
 export const themes: themeType[] = [
@@ -24,6 +25,7 @@ export const rooms: roomType[] = [
   "Living Room",
   "Dining Room",
   "Office",
+  "Kitchen",
   "Bedroom",
   "Bathroom",
   "Gaming Room",
