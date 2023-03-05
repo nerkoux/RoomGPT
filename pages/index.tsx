2595f0caef3e1bf3dbb1aa2e7c5896a595bac606
Nerko Dev <akshatmehta_14434@mpsjaipur.com>
2023-03-05 14:46:41 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>RoomGPT Forked Version</title>
+        <title>RoomGPT</title>
       </Head>
 
       <Header />
